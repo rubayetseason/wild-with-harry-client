c8e3b51fe0865673342d129ee2dffc7335b4bda2
rubayet-islam <season.ss2001@gmail.com>
2022-11-08 14:48:01 +0600

    react photo view feature added

--- src/components/pages/Service.js
+++ src/components/pages/Service.js
@@ -1,31 +1,33 @@
-import { Button, Card } from 'flowbite-react';
-import React from 'react';
+import { Button, Card } from "flowbite-react";
+import React from "react";
+import { PhotoProvider, PhotoView } from "react-photo-view";
+import "react-photo-view/dist/react-photo-view.css";
 
-const Service = ({service}) => {
-    const {name, img, details, price, _id} = service;
-    return (
-        <div className="max-w-sm">
-  <Card
-    imgAlt="Meaningful alt text for an image that is not purely decorative"
-    imgSrc={img}
-  >
-    <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-      {name}
-    </h5>
-    <p className="font-normal text-gray-700 dark:text-gray-400">
-     {details.slice(0, 100)} ...
-    </p>
-    <p className='font-semibold'>
-        Price : $ {price}
-    </p>
-    <div>
-    <Button className='mx-auto' gradientDuoTone="pinkToOrange">
-      View Details
-    </Button>
+const Service = ({ service }) => {
+  const { name, img, details, price, _id } = service;
+  return (
+    <div className="max-w-sm">
+      <Card>
+        <PhotoProvider>
+          <PhotoView src={img}>
+            <img src={img} alt="" />
+          </PhotoView>
+        </PhotoProvider>
+        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          {name}
+        </h5>
+        <p className="font-normal text-gray-700 dark:text-gray-400">
+          {details.slice(0, 100)} ...
+        </p>
+        <p className="font-semibold">Price : $ {price}</p>
+        <div>
+          <Button className="mx-auto" gradientDuoTone="purpleToPink">
+            View Details
+          </Button>
+        </div>
+      </Card>
     </div>
-  </Card>
-</div>
-    );
+  );
 };
 
 export default Service;
